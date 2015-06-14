549d5bfa9ed8e3b19f7046deca921021b26af691
Whitney Hiemstra <whitcolorado@gmail.com>
2015-06-14 16:19:10 -0600

    Category and Idea models exist, drafted pending specs to remind me of TODO work

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe Category do
+  it 'has a title'
+
+  it 'only is valid if title is present'
+
+  it 'blocks saving if title is absent'
+
+  # Relationships
+
+  it 'has many ideas'
+
+  
+end
---
+++ spec/models/idea_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe Idea do
+  it 'has a description'
+
+  it 'must have a description to be valid'
+
+  it 'blocks a save if description is absent'
+
+  # Relationships
+
+  it 'belongs to a category'
+
+  it 'must belong to a category to be valid'
+
+  # ---
+
+  it 'belongs to a user'
+
+  it 'must belong to a user to be valid'
+end
